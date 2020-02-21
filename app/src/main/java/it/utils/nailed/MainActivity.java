5cc14d7d77d175ac6cc107dd7d41d5459c7834b2
collideorscopeapps <collideorscopeapps@gmail.com>
2020-02-21 20:39:38 +0100

    dark theme

--- app/src/main/java/it/utils/nailed/MainActivity.java
+++ app/src/main/java/it/utils/nailed/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
+
         this.cameraManager = new CameraManager(this, this);
 
         resetTimer();
